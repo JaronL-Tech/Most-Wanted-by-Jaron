3451d3480086eb81dc370a94d6aea0f36d3c7eec
Jaron Leach <jaronleach98@gmail.com>
2023-10-20 12:26:23 -0400

    Edit and fix syntax errors.

--- app.js
+++ app.js
@@ -1,238 +1,329 @@
-
 function app(people) {
-    displayWelcome();
-    runSearchAndMenu(people);
-    return exitOrRestart(people);
+  displayWelcome();
+  runSearchAndMenu(people);
+  return exitOrRestart(people);
 }
 
 function displayWelcome() {
-    alert('Hello and welcome to the Most Wanted search application!');
+  alert("Hello and welcome to the Most Wanted search application!");
 }
 
 function runSearchAndMenu(people) {
-    const searchResults = searchPeopleDataSet(people);
+  const searchResults = searchPeopleDataSet(people);
 
-    if (searchResults.length > 1) {
-        displayPeople('Search Results', searchResults);
-    }
-    else if (searchResults.length === 1) {
-        const person = searchResults[0];
-        mainMenu(person, people);
-    }
-    else {
-        alert('No one was found in the search.');
-    }
+  if (searchResults.length > 1) {
+    displayPeople("Search Results", searchResults);
+  } else if (searchResults.length === 1) {
+    const person = searchResults[0];
+    mainMenu(person, people);
+  } else {
+    alert("No one was found in the search.");
+  }
 }
 
 function searchPeopleDataSet(people) {
+  const searchTypeChoice = validatedPrompt(
+    "Please enter in what type of search you would like to perform.",
+    ["id", "name", "traits"]
+  );
 
-    const searchTypeChoice = validatedPrompt(
-        'Please enter in what type of search you would like to perform.',
-        ['id', 'name', 'traits']
-    );
+  let results = [];
+  switch (searchTypeChoice) {
+    case "id":
+      results = searchById(people);
+      break;
+    case "name":
+      results = searchByName(people);
+      break;
+    case "traits":
+      //! TODO
+      results = searchByTraits(people);
+      break;
+    default:
+      return searchPeopleDataSet(people);
+  }
 
-    let results = [];
-    switch (searchTypeChoice) {
-        case 'id':
-            results = searchById(people);
-            break;
-        case 'name':
-            results = searchByName(people);
-            break;
-        case 'traits':
-            //! TODO
-            results = searchByTraits(people);
-            break;
-        default:
-            return searchPeopleDataSet(people);
-    }
-
-    return results;
+  return results;
 }
 
 function searchById(people) {
-    const idToSearchForString = prompt('Please enter the id of the person you are searching for.');
-    const idToSearchForInt = parseInt(idToSearchForString);
-    const idFilterResults = people.filter(person => person.id === idToSearchForInt);
-    return idFilterResults;
+  const idToSearchForString = prompt(
+    "Please enter the id of the person you are searching for."
+  );
+  const idToSearchForInt = parseInt(idToSearchForString);
+  const idFilterResults = people.filter(
+    (person) => person.id === idToSearchForInt
+  );
+  return idFilterResults;
 }
 
 function searchByName(people) {
-    const firstNameToSearchFor = prompt('Please enter the the first name of the person you are searching for.');
-    const lastNameToSearchFor = prompt('Please enter the the last name of the person you are searching for.');
-    const fullNameSearchResults = people.filter(person => (person.firstName.toLowerCase() === firstNameToSearchFor.toLowerCase() && person.lastName.toLowerCase() === lastNameToSearchFor.toLowerCase()));
-    return fullNameSearchResults;
+  const firstNameToSearchFor = prompt(
+    "Please enter the the first name of the person you are searching for."
+  );
+  const lastNameToSearchFor = prompt(
+    "Please enter the the last name of the person you are searching for."
+  );
+  const fullNameSearchResults = people.filter(
+    (person) =>
+      person.firstName.toLowerCase() === firstNameToSearchFor.toLowerCase() &&
+      person.lastName.toLowerCase() === lastNameToSearchFor.toLowerCase()
+  );
+  return fullNameSearchResults;
 }
 function searchByTraits(people) {
-    const traitsTOSearchForString = validatedPrompt('Please enter one of the listed traits of the person you are searching for.', ["eyeColor", 'height', 'weight', 'gender', 'occupation', 'quit']);
-    switch (traitsTOSearchForString){
-        case "eyecolor":
-            let peopleEyeColor = findPersonEyeColor(people)
-            displayPeople("People by Eye Color", peopleEyeColor);
-            break;
-        case "height":
-            let peopleheight = findPersonheight(people)
-            displayPeople("People by Height", peopleheight);
-            break;
-        case "weight":
-            let peopleweight = findPersonweight(people)
-            displayPeople("People by Weight", peopleweight);
-            break;
-        case "gender":
-            let peopleGender = findPersonGender(people)
-            displayPeople("People by Gender", peopleGender);
-            break;
-        case "occupation":
-            let peopleOccupation = findPersonOccupation(people)
-            displayPeople("People by Occupation", peopleOccupation);
-            break;
-        case "quit":
-            return;
-        default:
-            alert('Invalid input. Please try again.');
-            return searchByTraits(people);
+  const traitsTOSearchForString = validatedPrompt(
+    "Please enter one of the listed traits of the person you are searching for.",
+    ["eyeColor", "height", "weight", "gender", "occupation", "quit"]
+  );
+  switch (traitsTOSearchForString) {
+    case "eyecolor":
+      let peopleEyeColor = findPersonEyeColor(people);
+      displayPeople("People by Eye Color", peopleEyeColor);
+      break;
+    case "height":
+      let peopleheight = findPersonheight(people);
+      displayPeople("People by Height", peopleheight);
+      break;
+    case "weight":
+      let peopleweight = findPersonweight(people);
+      displayPeople("People by Weight", peopleweight);
+      break;
+    case "gender":
+      let peopleGender = findPersonGender(people);
+      displayPeople("People by Gender", peopleGender);
+      break;
+    case "occupation":
+      let peopleOccupation = findPersonOccupation(people);
+      displayPeople("People by Occupation", peopleOccupation);
+      break;
+    case "quit":
+      return;
+    default:
+      alert("Invalid input. Please try again.");
+      return searchByTraits(people);
   }
-  
 }
 
 function mainMenu(person, people) {
+  const mainMenuUserActionChoice = validatedPrompt(
+    `Person: ${person.firstName} ${person.lastName}\n\nDo you want to know their full information, family, or descendants?`,
+    ["info", "family", "descendants", "quit"]
+  );
 
-    const mainMenuUserActionChoice = validatedPrompt(
-        `Person: ${person.firstName} ${person.lastName}\n\nDo you want to know their full information, family, or descendants?`,
-        ['info', 'family', 'descendants', 'quit']
-    );
+  switch (mainMenuUserActionChoice) {
+    case "info":
+      //! TODO
+      let PersonInfo = PersonInfo(person);
+      displayPersonInfo(person);
+      console.log("Display person information.");
+      break;
+    case "family":
+      //! TODO
+      let personFamily = findpersonFamily(person, people);
+      displayPeople("Family", personFamily);
+      console.log("Display family infomation.");
+      break;
+    case "descendants":
+      //! TODO
+      let personDescendants = findpersonDescendants(person, people);
+      displayPeople("Descendants", personDescendants);
+      console.log("Display infomation of Descendants.");
+      break;
+    case "quit":
+      return;
+    default:
+      alert("Invalid input. Please try again.");
+  }
 
-    switch (mainMenuUserActionChoice) {
-        case "info":
-            //! TODO
-            let PersonInfo = findPersonInfo(person)
-             displayPersonInfo(person);
-             console.log('Display person information.');
-            break;
-        case "family":
-            //! TODO
-             let personFamily = findPersonFamily(person, people);
-             displayPeople('Family', personFamily);
-             console.log('Display family infomation.');
-            break;
-        case "descendants":
-            //! TODO
-             let personDescendants = findPersonDescendants(person, people);
-             displayPeople('Descendants', personDescendants);
-             console.log('Display infomation of Descendants.');
-            break;
-        case "quit":
-            return;
-        default:
-            alert('Invalid input. Please try again.');
-    }
-
-    return mainMenu(person, people);
+  return mainMenu(person, people);
+}
+function findPersonFamily(person, people) {
+  let personFamily = [];
+  const spouse = findSpouse(person, people);
+  if (spouse != undefined) {
+    personFamily.push(spouse);
+  }
+  const parents = findParents(person, people);
+  personFamily = personFamily.concat(parents);
+  const siblings = FindSiblings(person, people);
+  personFamily = personFamily.concat(siblings);
+  return personFamily;
+}
+function displayPersonInfo(person, people) {
+  const spouse = findSpouse(person, people);
+  const parents = findParents(person, people);
+  const spouseName = "";
+  if (spouse != undefined) {
+    spouseName = `${spouse.firstName} ${spouse.lastName}`;
+  }
+  const parentsNames = parents
+    .map((person) => `${person.firstName} ${person.lastName}`)
+    .join(", ");
+  alert(`Name: ${person.firstName} ${person.lastName} \n Gender:${person.gender} \n Date of Birth: ${person.dob} \n Height: ${person.height}
+    Weight: ${person.weight} \n Eyecolor: ${person.eyeColor} \n Occupation: ${person.occupation}
+    Parents: ${parentsNames}
+    Spouse: ${spouseName} `);
+}
+function findSpouse(person, people) {
+  const spouseid = person.currentSpouse;
+  const spouseArray = people.filter((person) => person.id === spouseid);
+  const spouse = spouseArray[0];
+  return spouse;
+}
+function findParents(person, people) {
+  const parentids = person.parents;
+  const parents = people.filter(
+    (person) => person.id === parentids[0] || person.id === parentids[1]
+  );
+  return parents;
+}
+function FindSiblings(person, people) {
+  const parentids = person.parents;
+  const siblings = people.filter((person) => person.parents === parentids);
+  return siblings;
+}
+function findPersonDescendants(person, people) {
+  const personid = person.id;
+  const descendants = people.filter(
+    (person) => person.parents[0] === personid || person.parents[1] === personid
+  );
+  return descendants;
 }
 
+function findPersonEyeColor(people) {
+  let eyeColorValue = validatedPrompt(
+    "What eye color would you like to search for? ",
+    ["brown", "blue", "hazel", "black", "green"]
+  );
+  let peopleByEyeColor = people.filter(function (person) {
+    if (person.eyeColor === eyeColorValue) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return peopleByEyeColor;
+}
+
+function findPersonGender(people) {
+  let GenderValue = validatedPrompt(
+    "What Gender would you like to search for? ",
+    ["Male", "Female"]
+  );
+  let PeoplebyGender = people.filter(function (person) {
+    if (person.Gender === GenderValue) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return PeoplebyGender;
+}
+
+function findPersonheight(people) {
+  let HeightValue = parseInt(
+    "What height are you looking for? ",
+    [
+      71, 65, 76, 62, 70, 72, 66, 59, 69, 76, 74, 66, 58, 62, 61, 74, 71, 70,
+      63, 70, 67, 67,
+    ]
+  );
+  let Peoplebyheight = people.filter(function (person) {
+    if (person.height === HeightValue) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return Peoplebyheight;
+}
+
+{
+  function findPersonweight(people) {
+    let WeightValue = parseInt(
+      "Enter weight? ",
+      [
+        175, 162, 250, 115, 207, 256, 170, 137, 199, 205, 118, 179, 156, 235,
+        112, 184, 249, 187, 241, 110, 100, 100,
+      ]
+    );
+    let Peoplebyweight = people.filter(function (person) {
+      if (person.Weight === WeightValue) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return Peoplebyweight;
+  }
+}
+{
+  function findPersonOccupation(people) {
+    let OccupationValue = validatedPrompt(
+      "What occupation are you looking for?",
+      [
+        "Progammer",
+        "assistant",
+        "landscaper",
+        "Nurse",
+        "Student",
+        "architect",
+        "Doctor",
+        "Politician",
+      ]
+    );
+    let Peoplebyoccupation = people.filter(function (person) {
+      if (person.Occupation === OccupationValue) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    return Peoplebyoccupation;
+  }
+}
 function displayPeople(displayTitle, peopleToDisplay) {
-    const formatedPeopleDisplayText = peopleToDisplay.map(person => `${person.firstName} ${person.lastName}`).join('\n');
-    alert(`${displayTitle}\n\n${formatedPeopleDisplayText}`);
+  const formatedPeopleDisplayText = peopleToDisplay
+    .map((person) => `${person.firstName} ${person.lastName}`)
+    .join("\n");
+  alert(`${displayTitle}\n\n${formatedPeopleDisplayText}`);
 }
 
 function validatedPrompt(message, acceptableAnswers) {
-    acceptableAnswers = acceptableAnswers.map(aa => aa.toLowerCase());
+  acceptableAnswers = acceptableAnswers.map((aa) => aa.toLowerCase());
 
-    const builtPromptWithAcceptableAnswers = `${message} \nAcceptable Answers: ${acceptableAnswers.map(aa => `\n-> ${aa}`).join('')}`;
+  const builtPromptWithAcceptableAnswers = `${message} \nAcceptable Answers: ${acceptableAnswers
+    .map((aa) => `\n-> ${aa}`)
+    .join("")}`;
 
-    const userResponse = prompt(builtPromptWithAcceptableAnswers).toLowerCase();
+  const userResponse = prompt(builtPromptWithAcceptableAnswers).toLowerCase();
 
-    if (acceptableAnswers.includes(userResponse)) {
-        return userResponse;
-    }
-    else {
-        alert(`"${userResponse}" is not an acceptable response. The acceptable responses include:\n${acceptableAnswers.map(aa => `\n-> ${aa}`).join('')} \n\nPlease try again.`);
-        return validatedPrompt(message, acceptableAnswers);
-    }
+  if (acceptableAnswers.includes(userResponse)) {
+    return userResponse;
+  } else {
+    alert(
+      `"${userResponse}" is not an acceptable response. The acceptable responses include:\n${acceptableAnswers
+        .map((aa) => `\n-> ${aa}`)
+        .join("")} \n\nPlease try again.`
+    );
+    return validatedPrompt(message, acceptableAnswers);
+  }
 }
 
 function exitOrRestart(people) {
-    const userExitOrRestartChoice = validatedPrompt(
-        'Would you like to exit or restart?',
-        ['exit', 'restart']
-    );
+  const userExitOrRestartChoice = validatedPrompt(
+    "Would you like to exit or restart?",
+    ["exit", "restart"]
+  );
 
-    switch (userExitOrRestartChoice) {
-        case 'exit':
-            return;
-        case 'restart':
-            return app(people);
-        default:
-            alert('Invalid input. Please try again.');
-            return exitOrRestart(people);
-    }
-
-}
-
-function findPersonEyeColor(people){
-    let eyeColorValue = validatedPrompt("What eye color would you like to search for? ", ["brown", "blue", "hazel", "black", "green"])
-    let peopleByEyeColor = people.filter(function (person) {
-        if (person.eyeColor === eyeColorValue) {
-            return true;
-        } else {
-            return false;
-        }
-    })
-    return peopleByEyeColor;
-}
-
-{   
-    function findPersonGender(people){
-    let GenderValue = validatedPrompt("What Gender would you like to search for? ", ["Male", "Female"])
-    let PeoplebyGender = people.filter(function (person) {
-        if (person.Gender === GenderValue) {
-            return true;
-        } else {
-            return false;
-        }
-    })
-    return PeoplebyGender;}
-{
-   
-{
-    function findPersonheight(people){
-    let HeightValue = parseInt("What height are you looking for? ", [71, 65, 76, 62, 70, 72, 66, 59, 69, 76, 74, 66, 58, 62, 61, 74, 71, 70, 63, 70, 67, 67])
-    let Peoplebyheight = people.filter(function (person) {
-        if (person.height === HeightValue) {
-            return true;
-        } else {
-            return false;
-        }
-    })
-    return Peoplebyheight;
-}
-    
-    {
-        function findPersonweight(people){
-            let WeightValue = parseInt("Enter weight? ", [175, 162, 250, 115, 207, 256, 170, 137, 199, 205, 118, 179, 156, 235, 112, 184, 249, 187, 241, 110, 100, 100 ])
-            let Peoplebyweight = people.filter(function (person) {
-                if (person.Weight === WeightValue) {
-                    return true;
-                } else {
-                    return false;
-                }
-            })
-            return Peoplebyweight;
-    }
-}
-{ 
-    function findPersonOccupation(people){
-        let OccupationValue = validatedPrompt("What occupation are you looking for?", ["Progammer", "assistant", "landscaper", "Nurse", "Student", "architect","Doctor", "Politician" ])
-        let Peoplebyoccupation = people.filter(function (person){
-            if (person.Occupation === OccupationValue) {
-                return true;
-            } else {
-                return false;
-            }
-        })
-        return Peoplebyoccupation;
-    }
-
-} {
-    
+  switch (userExitOrRestartChoice) {
+    case "exit":
+      return;
+    case "restart":
+      return app(people);
+    default:
+      alert("Invalid input. Please try again.");
+      return exitOrRestart(people);
+  }
 }
